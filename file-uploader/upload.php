1e280ee1443d298a1c00cbdf6d93308d8e7e7933
Akshay1705 <akstyle1705@gmail.com>
2025-06-13 21:38:32 +0530

    Refactor code structure for improved readability and maintainability

--- file-uploader/upload.php
+++ file-uploader/upload.php
@@ -1,16 +1,20 @@
 <?php
 $uploadDir = "uploads/";
 $allowedTypes = ['pdf', 'doc', 'docx'];
-$maxFileSize = 2 * 1024 * 1024; // 2MB
+$maxFileSize = 2 * 1024 * 1024; // 2MB => 1kb = 1024 (and) 1MB = 1024 * 1024 .
 
 echo '<link rel="stylesheet" href="style.css">';
 echo '<div class="container">';
 
 if (isset($_FILES["resume"])) {
-    $fileName = $_FILES["resume"]["name"];
-    $tmpName = $_FILES["resume"]["tmp_name"];
-    $fileSize = $_FILES["resume"]["size"];
+    $fileName = $_FILES["resume"]["name"];//$_FILES is a superglobal ["input name"]["file name"]
+    $tmpName = $_FILES["resume"]["tmp_name"];//["input name"]["temporary file name"]
+    $fileSize = $_FILES["resume"]["size"];//["input name"]["file size in bytes"]
     $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    // Extracts the file extension (like pdf, doc, or docx) from the original file name.
+    // pathinfo() => breaks down the filename into parts.
+    // PATHINFO_EXTENSION => specifically gets the extension
+    // strtolower() ==> converts it to lowercase to avoid issues like( .PDF vs .pdf.)
 
     // Validate file type
     if (!in_array($fileExt, $allowedTypes)) {
